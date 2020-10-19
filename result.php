8c849b438bc241b6b43932b29c3d7231afd47db3
Reginaldo Rubens da Silva <reginaldorubens@gmail.com>
2020-10-19 09:08:56 -0300

    Refactoring to clean code

--- result.php
+++ result.php
@@ -24,41 +24,29 @@
 <?php 
 	$link = mysqli_connect("localhost","root","","Search");
 	
-	if(isset($_GET['search'])){
-	
-	$get_value = $_GET['user_query'];
-	
-	if($get_value==''){
-	
-	echo "<center><b>Please go back, and write something in the search box!</b></center>";
-	exit();
-	}
-	
-	$result_query = "select * from sites where site_keywords like '%$get_value%'";
-	
-	$run_result = mysqli_query($link, $result_query);
-	
-	
-	while($row_result=mysqli_fetch_array($run_result,MYSQLI_BOTH)){
-		
-		$site_title=$row_result['site_title'];
-		$site_link=$row_result['site_link'];
-		$site_desc=$row_result['site_desc'];
-		$site_image=$row_result['site_image'];
-	
-	echo "<div class='results'>
-		
-		<h2>$site_title</h2>
-		<a href='$site_link' target='_blank'>$site_link</a>
-		<p align='justify'>$site_desc</p> 
-		<img src='images/$site_image' width='100' height='100' />
-		
-		</div>";
+	if(isset($_GET['search'])){	
+		$user_query = $_GET['user_query'];
 
+		if($user_query == ''){
+			echo "<center><b>Please go back, and write something in the search box!</b></center>";
+			exit();
 		}
-}
 
+		$result_query = "select * from sites where site_keywords like '%$user_query'";
 
+		$run_result = mysqli_query($link, $result_query);
+
+		while($row_result=mysqli_fetch_array($run_result,MYSQLI_BOTH)){
+			echo "
+				<div class='results'>
+					<h2>{$row_result['site_title']}</h2>
+					<a href='{$row_result['site_link']}' target='_blank'>{$row_result['site_link']}</a>
+					<p align='justify'>{$row_result['site_desc']}</p> 
+					<img src='images/{$row_result['site_image']}' width='100' height='100' />
+				</div>
+			";
+		}
+	}
 ?>
 
 
